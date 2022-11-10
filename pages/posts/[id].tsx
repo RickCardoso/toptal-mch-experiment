2fa4636750e54dcacfe3f61f35e16a9a1a948481
Ricardo Cardoso <ricardo.cardoso@nexar.app>
2022-11-10 14:46:55 -0300

    feat: better layout

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,7 @@
 import {fetcher} from "../../utils/http";
 import useSWR from "swr";
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 const PostPage = () => {
   const router = useRouter();
@@ -11,9 +12,12 @@ const PostPage = () => {
   if (!data) return <div>loading...</div>
 
   return (
-    <div>
-      <h1>{data.title}</h1>
+    <div className="p-6">
+      <h1 className="text-lg font-semibold mb-4">{data.title}</h1>
       <p>{data.content}</p>
+      <Link href="/posts">
+        <button className="bg-transparent hover:bg-white hover:bg-opacity-50 border border-white py-2 px-4 rounded-md mt-8">Back to posts</button>
+      </Link>
     </div>
   );
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,48 +4,54 @@ import { useForm } from "react-hook-form"
 import useSWR from "swr"
 import { fetcher } from "../../utils/http"
 import {Post} from "../../types/post";
+import {useState} from "react";
 
 const PostsPage = () => {
-  const { data, error, mutate } = useSWR('/api/posts', fetcher)
+  const { data, error, mutate } = useSWR('/api/posts', fetcher);
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
   const onSubmit = (data: Partial<Post>) => axios.post('/api/posts', data).then(() => {
     void mutate();
     reset();
   });
 
+  const [addPostOpen, setAddPostOpen] = useState(false);
+
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
 
   return (
-    <div className="max-w-screen-xl mx-auto p-6">
-      <h1 className="text-xl font-semibold mb-4">Posts</h1>
-      <form className="max-w-4xl flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
+    <>
+      <div className={`p-6 ${addPostOpen ? 'pr-72 mr-72' : ''} transition-all`}>
+        <button className="bg-purple-600 hover:bg-purple-400 py-2 px-4 rounded-md" type="button" onClick={() => setAddPostOpen(state => !state)}>Add new post</button>
+
+        <h1 className="text-lg font-semibold my-4">Posts</h1>
+        <ul className="flex flex-col gap-4">
+          {data.map((post: Post) => (
+            <Link href={`/posts/${post.id}`} key={post.id}>
+              <li className="rounded-xl bg-zinc-300 p-6 text-black">
+                <h3 className="text-lg text-semibold mb-2">{post.title}</h3>
+                <p className="text-ellipsis overflow-hidden whitespace-nowrap">{post.content}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      {addPostOpen && <div className="fixed top-0 bottom-0 left-0 right-0 bg-black bg-opacity-80" onClick={() => setAddPostOpen(state => !state)} />}
+      <form className={`max-w-xl w-full flex flex-col gap-2 fixed left-full top-0 bottom-0 ${addPostOpen ? '-translate-x-full' : ''} transition-all p-6 bg-black`} onSubmit={handleSubmit(onSubmit)}>
         <h2 className="text-lg">Add new post</h2>
         <div className="flex flex-col">
           <label htmlFor="title">Title</label>
-          <input type="text" {...register('title')} />
+          <input className="h-10 rounded-md px-3" type="text" {...register('title')} />
         </div>
         <div className="flex flex-col">
           <label htmlFor="content">Content</label>
-          <textarea {...register('content')} />
+          <textarea className="rounded-md px-3 py-2" {...register('content')} rows={10} />
         </div>
-        <div className="text-right">
-          <button className="bg-purple-600 hover:bg-purple-400 py-1 px-2">Submit</button>
+        <div className="text-right mt-4">
+          <button className="bg-purple-600 hover:bg-purple-400 py-2 px-4 rounded-md">Submit</button>
         </div>
       </form>
-
-      <h2 className="text-lg font-semibold mb-4">Posts</h2>
-      <ul className="flex flex-col gap-4">
-        {data.map((post: Post) => (
-          <Link href={`/posts/${post.id}`} key={post.id}>
-            <li className="rounded-xl bg-zinc-300 p-6 text-black">
-              <h3 className="text-lg text-semibold mb-2">{post.title}</h3>
-              <p className="text-ellipsis overflow-hidden whitespace-nowrap">{post.content}</p>
-            </li>
-          </Link>
-        ))}
-      </ul>
-    </div>
+    </>
   )
 }
 
